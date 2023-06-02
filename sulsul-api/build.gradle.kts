8da676a07989b02107ac10c53cc0a66b8d302327
bohub12 <qhrb1997@naver.com>
2023-06-02 23:52:48 +0900

    fix. add mongodb & springdoc-ui dependency

--- sulsul-api/build.gradle.kts
+++ sulsul-api/build.gradle.kts
@@ -3,6 +3,13 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
 //    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // mongodb
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+
+    // swagger
+    // springdoc-openapi
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
 }
 
 tasks {
