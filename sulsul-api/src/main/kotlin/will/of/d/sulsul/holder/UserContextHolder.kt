9c7c3a8d48bd80094496f0f70d8087538cb00125
seongchan.hwang <dbfpzk142@gmail.com>
2023-06-11 16:41:41 +0900

    feat: add user argument resolver
    https://oss.navercorp.com/ai-assistant-platform/issues/issues/

---
+++ sulsul-api/src/main/kotlin/will/of/d/sulsul/holder/UserContextHolder.kt
@@ -0,0 +1,11 @@
+package will.of.d.sulsul.holder
+
+import will.of.d.sulsul.user.User
+
+object UserContextHolder {
+    private val holder = ThreadLocal<User>()
+
+    fun get(): User? = holder.get()
+    fun set(user : User) = holder.set(user)
+    fun remove() = holder.remove()
+}
