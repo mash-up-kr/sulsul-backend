8911198495c69459910a41e847a29974d1cd91b1
bohub12 <qhrb1997@naver.com>
2023-06-12 00:03:25 +0900

    feat. enable mongo auditing (createdDate, updatedDate)

--- sulsul-domain/src/main/kotlin/will/of/d/sulsul/alcohol/enroll/domain/EnrollAlcohol.kt
+++ sulsul-domain/src/main/kotlin/will/of/d/sulsul/alcohol/enroll/domain/EnrollAlcohol.kt
@@ -1,8 +1,11 @@
 package will.of.d.sulsul.alcohol.enroll.domain
 
 import org.bson.types.ObjectId
+import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
 
 @Document(collection = "enroll_alcohol")
 data class EnrollAlcohol(
@@ -11,7 +14,10 @@ data class EnrollAlcohol(
     val userId: ObjectId,
     val sojuCount: Int,
     val beerCount: Int,
-    val alcoholAmount: Double = 0.0
+    val alcoholAmount: Double = 0.0,
 
     // TODO : 주량 정확히 정해지면 각 주종별 개수 담아두는 필드 추가하기
+
+    @CreatedDate var createdDate: LocalDateTime? = null,
+    @LastModifiedDate var updatedDate: LocalDateTime? = null
 )
