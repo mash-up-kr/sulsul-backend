9c1fd8a3c253680f0412f8e4cbdd5bd26d273b43
bohub12 <qhrb1997@naver.com>
2023-06-16 00:09:44 +0900

    fix. not to go through userService

--- sulsul-api/src/main/kotlin/will/of/d/sulsul/alcohol/drinkingLimit/controller/DrinkingLimitController.kt
+++ sulsul-api/src/main/kotlin/will/of/d/sulsul/alcohol/drinkingLimit/controller/DrinkingLimitController.kt
@@ -9,25 +9,18 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import will.of.d.sulsul.alcohol.drinkingLimit.dto.request.PostDrinkingLimitReq
 import will.of.d.sulsul.alcohol.drinkingLimit.service.DrinkingLimitService
-import will.of.d.sulsul.exception.UserNotFoundException
 import will.of.d.sulsul.user.User
-import will.of.d.sulsul.user.UserService
 
 @Tag(name = "주량등록 컨트롤러")
 @RestController
 @RequestMapping("/api/drinkingLimit")
 class DrinkingLimitController(
-    private val drinkingLimitService: DrinkingLimitService,
-    private val userService: UserService
+    private val drinkingLimitService: DrinkingLimitService
 ) {
 
     @Operation(summary = "주량 등록 API", description = "로그인 시, 주량을 등록할 때 호출하는 API")
     @PostMapping("")
-    fun saveEnrollAlcohol(@RequestBody body: PostDrinkingLimitReq, user: User): ResponseEntity<Any> {
-        userService.getUser(user.kakaoUserId)?.let {
-            return ResponseEntity.ok(drinkingLimitService.save(it.kakaoUserId, body))
-        }
-
-        throw UserNotFoundException("해당 토큰에 대한 유저 정보 없음")
+    fun save(@RequestBody body: PostDrinkingLimitReq, user: User): ResponseEntity<Any> {
+        return ResponseEntity.ok(drinkingLimitService.save(user.kakaoUserId, body))
     }
 }
