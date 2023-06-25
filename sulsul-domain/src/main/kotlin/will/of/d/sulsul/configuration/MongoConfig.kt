e7bacd034c52e115a2c66ff95290572e07b28096
bohub12 <qhrb1997@naver.com>
2023-06-25 21:21:16 +0900

    chore: mongo auditing

--- sulsul-domain/src/main/kotlin/will/of/d/sulsul/configuration/MongoConfig.kt
+++ sulsul-domain/src/main/kotlin/will/of/d/sulsul/configuration/MongoConfig.kt
@@ -1,8 +1,10 @@
 package will.of.d.sulsul.configuration
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.config.EnableMongoAuditing
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @Configuration
+@EnableMongoAuditing
 @EnableMongoRepositories(basePackages = ["will.of.d.sulsul"])
 class MongoConfig
