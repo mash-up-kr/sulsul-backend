85bf72ce53723db55821e21e35aad67ac54cad61
SungMin Joo <59276134+god9599@users.noreply.github.com>
2023-06-27 20:15:55 +0900

    feat: drinking report swagger

--- sulsul-api/src/main/kotlin/will/of/d/sulsul/alcohol/drinkingMeasurement/dto/request/DrinkingMeasurementReq.kt
+++ sulsul-api/src/main/kotlin/will/of/d/sulsul/alcohol/drinkingMeasurement/dto/request/DrinkingMeasurementReq.kt
@@ -1,19 +1,24 @@
 package will.of.d.sulsul.alcohol.drinkingMeasurement.dto.request
 
+import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 import will.of.d.sulsul.alcohol.drinkingMeasurement.vo.DrinkingAmountVO
 import java.time.LocalDateTime
 
 data class DrinkingMeasurementReq(
+    @Schema(description = "술 종류와 잔 수", example = "[{\"drinkType\":\"소주\", \"glasses\":4}]")
     @field:NotBlank(message = "측정한 내역을 입력해주세요.")
     val drinks: List<DrinkingAmountVO>,
 
+    @Schema(description = "술을 마신 시작 시간", example = "2021-08-20T15:00:00")
     @field:NotBlank(message = "술자리가 시작한 시간을 입력해주세요.")
     val drinkingStartTime: LocalDateTime,
 
+    @Schema(description = "술을 마신 종료 시간", example = "2021-08-20T18:20:00")
     @field:NotBlank(message = "술자리가 끝난 시간을 입력해주세요.")
     val drinkingEndTime: LocalDateTime,
 
+    @Schema(description = "총 마신 잔 수", example = "4")
     @field:NotBlank(message = "유저가 마신 총 잔 수를 입력해주세요.")
     val totalDrinkGlasses: Int
 )
--- sulsul-api/src/main/kotlin/will/of/d/sulsul/alcohol/drinkingMeasurement/dto/response/DrinkingMeasurementRes.kt
+++ sulsul-api/src/main/kotlin/will/of/d/sulsul/alcohol/drinkingMeasurement/dto/response/DrinkingMeasurementRes.kt
@@ -1,16 +1,24 @@
 package will.of.d.sulsul.alcohol.drinkingMeasurement.dto.response
 
+import io.swagger.v3.oas.annotations.media.Schema
 import will.of.d.sulsul.alcohol.drinkingMeasurement.domain.DrinkingMeasurement
 import will.of.d.sulsul.alcohol.drinkingMeasurement.domain.Drinks
 import java.time.LocalDateTime
 
 class DrinkingMeasurementRes(
+    @Schema(description = "식별 id", example = "6481c97405e8335a58bc4337")
     val id: String,
+    @Schema(description = "유저가 총 마신 술의 잔", example = "4")
     val totalDrinkGlasses: Int,
+    @Schema(description = "유저가 마신 술의 평균 알콜 도수", example = "16.9")
     val averageAlcoholContent: Double,
+    @Schema(description = "유저가 술을 마신 시간", example = "3시간 20분")
     val drinkingDuration: String,
+    @Schema(description = "유저가 마신 술의 칼로리", example = "399")
     val alcoholCalorie: Int,
+    @Schema(description = "유저가 마신 날짜", example = "2021-08-20T15:00:00")
     val drankAt: LocalDateTime,
+    @Schema(description = "유저가 마신 술의 종류와 잔 수", example = "[{\"drinkType\":\"소주\",\"glasses\":4}]")
     val drinks: List<Drinks>
 ) {
     companion object {
