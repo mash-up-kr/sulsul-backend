ff7b17b8fb66e50a718f5d26e8ef4c3f87d770fb
bohub12 <qhrb1997@naver.com>
2023-07-01 00:52:36 +0900

    hotfix: swagger issue #50

--- sulsul-api/src/main/kotlin/will/of/d/sulsul/config/SwaggerConfig.kt
+++ sulsul-api/src/main/kotlin/will/of/d/sulsul/config/SwaggerConfig.kt
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
+import io.swagger.v3.oas.models.servers.Server
 import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -39,6 +40,7 @@ class SwaggerConfig {
             )
 
         return OpenAPI()
+            .addServersItem(Server().url("/"))
             .addSecurityItem(securityRequirement)
             .components(components)
     }
