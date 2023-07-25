4b86d23ad05e879e051bd92ab992df660ff6b49e
plzprayme <dbfpzk142@gmail.com>
2023-07-26 00:23:35 +0900

    feat: Change alcohol amount of titles

--- sulsul-domain/src/main/kotlin/will/of/d/sulsul/title/domain/Title.kt
+++ sulsul-domain/src/main/kotlin/will/of/d/sulsul/title/domain/Title.kt
@@ -9,12 +9,13 @@ enum class Title(
     val subText: String,
     val badgeImageUrl: String,
     val cardImageUrl: String,
-    val alcoholAmount: Double
+    val minAlcoholAmount: Double,
+    val maxAlcoholAmound: Double
 ) {
-    BRONZE("술요미", "귀엽네", mockImageUrl, mockImageUrl, Drink.SOJU.alcoholAmountPerGlass * 7),
-    SILVER("술반인", "가자~", mockImageUrl, mockImageUrl, Drink.SOJU.alcoholAmountPerGlass * 15),
-    GOLD("이쯤되면 술잘알", "술 좀 치네", mockImageUrl, mockImageUrl, Drink.SOJU.alcoholAmountPerGlass * 23),
-    PLATINUM("알낳괴", "미쳤다", mockImageUrl, mockImageUrl, Drink.SOJU.alcoholPercentage * 31),
-    DIAMOND("음주가무 천상계", "알콜 마스터", mockImageUrl, mockImageUrl, Drink.SOJU.alcoholPercentage * 40),
-    MASTER("Alcohol God", "알콜 마스터", mockImageUrl, mockImageUrl, Drink.SOJU.alcoholPercentage * 50_000)
+    BRONZE("술요미", "귀엽네", mockImageUrl, mockImageUrl, 0.0, Drink.SOJU.alcoholAmountPerGlass * 7),
+    SILVER("술반인", "가자~", mockImageUrl, mockImageUrl, BRONZE.maxAlcoholAmound, Drink.SOJU.alcoholAmountPerGlass * 15),
+    GOLD("이쯤되면 술잘알", "술 좀 치네", mockImageUrl, mockImageUrl, SILVER.maxAlcoholAmound, Drink.SOJU.alcoholAmountPerGlass * 23),
+    PLATINUM("알낳괴", "미쳤다", mockImageUrl, mockImageUrl, GOLD.maxAlcoholAmound, Drink.SOJU.alcoholPercentage * 31),
+    DIAMOND("음주가무 천상계", "알콜 마스터", mockImageUrl, mockImageUrl, PLATINUM.maxAlcoholAmound, Drink.SOJU.alcoholPercentage * 40),
+    MASTER("Alcohol God", "알콜 마스터", mockImageUrl, mockImageUrl, DIAMOND.maxAlcoholAmound, Drink.SOJU.alcoholPercentage * 50_000)
 }
