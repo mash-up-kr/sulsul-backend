0225886c3c30ca9fceb4713e24c8c6b302cdb5d5
plzprayme <dbfpzk142@gmail.com>
2023-07-30 03:57:42 +0900

    chore: Add schema allow values

--- sulsul-api/src/main/kotlin/will/of/d/sulsul/alcohol/drinkingLimit/dto/DrinkingLimitDto.kt
+++ sulsul-api/src/main/kotlin/will/of/d/sulsul/alcohol/drinkingLimit/dto/DrinkingLimitDto.kt
@@ -1,6 +1,9 @@
 package will.of.d.sulsul.alcohol.drinkingLimit.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class DrinkingLimitDto(
+    @Schema(description = "술의 종류", allowableValues = ["소주", "와인", "맥주", "위스키", "고량주"])
     val drinkType: String,
     val glass: Int
 )
